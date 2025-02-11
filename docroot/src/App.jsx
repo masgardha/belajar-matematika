f2afeabe07891e6ec6f63e37c05ea67e484f8cc3
masgardha <masgardha@gmail.com>
2025-02-11 12:21:30 +0700

    install react

---
+++ docroot/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SelectGame from './components/select-game/SelectGame'
+
+function App() {
+  return (
+    <>
+      <SelectGame />
+    </>
+  )
+}
+
+export default App
---
+++ docroot/src/components/select-game/SelectGame.jsx
@@ -0,0 +1,9 @@
+function SelectGame() {
+    return (
+        <div>
+            <h4 className="text-xs text-center text-blue-500">Pilih Game Tipe</h4>
+
+        </div>
+    )
+}
+export default SelectGame()
